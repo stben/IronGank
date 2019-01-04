f50a749b46d21598ce540ea5082d92bff0cca422
郑绪祺 <1194792327@qq.com>
2019-01-04 16:23:29 +0800

    Revert "Merge branch 'create_model_user' into 'master'"
    This reverts merge request !13

--- server/Student/models.py
+++ server/Student/models.py
@@ -1,12 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
-
-# 用户类，老师的用户名用email 学生的用手机号
-class User(AbstractUser):
-    sid = models.CharField(max_length=20, blank=True)
-
-    class Meta(AbstractUser.Meta):
-        verbose_name = '用户'
-        verbose_name_plural = verbose_name
-        ordering = ['-id']
+# Create your models here.
