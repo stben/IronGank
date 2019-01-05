5efa31b65a420ef7a9dfcddfbe0ff556ceedd41f
黄其轩 <hhqqxx11617@163.com>
2019-01-05 22:51:59 +0800

    This is index page of teacher. Ref #21

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import RoomManage from '@/components/RoomMange'
+import TeacherIndex from '@/components/TeacherIndex'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/TeacherIndex',
+      name: 'TeacherIndex',
+      component: TeacherIndex
+    },
+    {
+      path: '/RoomManage',
+      name: 'RoomManage',
+      component: RoomManage
     }
   ]
 })
