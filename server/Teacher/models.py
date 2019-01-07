6825cbe45067c04fbf31d7115a035e012532a3ea
胡孝腾 <948949782@qq.com>
2019-01-07 23:10:09 +0800

    This is the roomandteachertable's init model. Ref #32

--- server/Teacher/models.py
+++ server/Teacher/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    room_id = models.CharField(max_length=40, primary_key=True)
+    room_title = models.CharField(max_length=100)
+    college = models.CharFielsd(max_length=100)
+    password = models.CharFielsd(max_length=100)
+    room_description = models.CharFielsd(max_length=100)
+    is_need_whiteboard = models.BooleanField()
+    is_need_code_editor = models.BooleanField()
+    is_need_password = models.BooleanField()
+
+
+class College(models.Model):
+    college_name = models.CharField(max_length=100)
+    college_id = models.CharField(max_length=100, primary_key=True)
+
+
+class TimeTable(models.Model):
+    room_num = models.CharField(max_length=100, foreign_key=True)
+    class_week = models.IntegerField(max_length=10)
+    class_day = models.IntegerField(max_length=10)
+    class_class = models.IntegerField(max_length=10)
+
+
+class RoomAndTeacher(models.Model):
+    room_id = models.CharField(max_length=100, foreign_key=True)
+    username = models.CharField(max_length=100, foreign_key=True)
+    status = models.BooleanField()
