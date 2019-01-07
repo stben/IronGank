6715b86c46cd0376f6a852a4e265e4e9e4f342ba
胡孝腾 <948949782@qq.com>
2019-01-08 00:31:16 +0800

    This is the room and student table. Ref#33

--- server/Teacher/models.py
+++ server/Teacher/models.py
@@ -28,3 +28,7 @@ class RoomAndTeacher(models.Model):
     room_id = models.CharField(max_length=100, foreign_key=True)
     username = models.CharField(max_length=100, foreign_key=True)
     status = models.BooleanField()
+	
+class room_and_student(models.Model):
+    room_id = models.CharField(max_length=100, foreign_key=True)
+    username = models.CharField(max_length=100, foreign_key=True)	
