71375b1a0f5c0fb0684d6caa5504f6dce00c7642
黄其轩 <hhqqxx11617@163.com>
2019-01-08 17:11:48 +0800

    This is the page of room information manage. Ref #222

---
+++ frontend/src/components/RoomManage.vue
@@ -0,0 +1,41 @@
+<template xmlns="http://www.w3.org/1999/html">
+  <div>
+    <TeacherFrame :selected="'0'" :title="'房间 '+roomNo+'：直播信息管理'"></TeacherFrame>
+    <mu-paper class="paper2" z-depth="4">
+      <mu-text-field v-model="value" placeholder="请输入房间简介"></mu-text-field><br/>
+      <mu-text-field v-model="value2" placeholder="请输入房间标题"></mu-text-field><br/>
+      <mu-flex class="select-control-row">
+        <mu-switch v-model="switchVal.value1" label="白板"></mu-switch>
+      </mu-flex>
+      <mu-flex class="select-control-row">
+        <mu-switch v-model="switchVal.value2" label="代码编译区"></mu-switch>
+      </mu-flex>
+    </mu-paper>
+  </div>
+</template>
+
+<script>
+import TeacherFrame from '../components/TeacherFrame'
+import PickStuTabsAccepted from '../components/PickStuTabsAccepted'
+import Divider from 'muse-ui/lib'
+export default {
+  name: 'RoomManage',
+  components: {
+    Divider,
+    TeacherFrame,
+    PickStuTabsAccepted
+  },
+  data () {
+    return {
+      switchVal: {
+        value1: false,
+        value2: false
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
