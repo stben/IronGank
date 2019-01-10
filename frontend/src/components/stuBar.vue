b4e8179e2da3bd6a98499799fa326108c28f8e62
黄其轩 <hhqqxx11617@163.com>
2019-01-10 21:08:33 +0800

    Update the bar for students' pages. Ref #70

---
+++ frontend/src/components/stuBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <mu-appbar class="bar" color="primary">
+    <mu-button flat slot="left" to="/stuIndex">直播系统首页</mu-button>
+    {{title}}
+  </mu-appbar>
+</template>
+
+<script>
+export default {
+  name: 'stuBar',
+  props: ['title']
+}
+</script>
+
+<style scoped>
+  .bar{
+    margin: -60px 0;
+  }
+
+</style>
---
+++ frontend/src/components/stuFrame.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="stuFrame">
+    <stuBar v-bind:title="title"></stuBar>
+  </div>
+</template>
+
+<script>
+import stuBar from '../components/stuBar'
+export default {
+  name: 'stuFrame',
+  components: {
+    stuBar
+  },
+  props: ['selected', 'title']
+}
+</script>
+
+<style scoped>
+
+</style>
