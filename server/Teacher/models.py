8fba9109375444f3ff5425c3890e9dab1ebec2c1
占晨昊 <1405092011@qq.com>
2019-01-11 14:34:48 +0800

    Add unique key and change TandR bool into int field./ Ref #76

--- server/Teacher/models.py
+++ server/Teacher/models.py
@@ -4,12 +4,12 @@ from Student.models import Student
 
 
 class College(models.Model):
-    name = models.CharField('院系名',max_length=100)
-    college_id = models.CharField('院系号',max_length=100)
+    name = models.CharField('院系名', max_length=100, unique=True)
+    college_id = models.CharField('院系号', max_length=100, unique=True)
+
     class Meta:
         verbose_name = "院系信息"
         verbose_name_plural = "院系信息"
-
 
 
 class Room(models.Model):
@@ -39,7 +39,8 @@ class RoomAndTeacher(models.Model):
 class RoomAndStudent(models.Model):
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
-    status = models.BooleanField()
+    status = models.IntegerField()
+    # 0 表示已通过，1 表示申请中， 2表示已移除
 
 
 class ListOfForbiddenStudents(models.Model):
