92172f77fa2f8b01af961a1b9d16188edf029dc3
黄其轩 <hhqqxx11617@163.com>
2019-01-11 14:27:50 +0800

    Update the bar for teacheer and student page. Ref #74

--- frontend/src/components/TeacherBar.vue
+++ frontend/src/components/TeacherBar.vue
@@ -1,6 +1,7 @@
 <template>
   <mu-appbar class="bar" color="primary">
-    <mu-button flat slot="left" to="/">教师直播系统</mu-button>
+    <mu-button flat slot="left" to="/TeacherIndex">教师直播系统</mu-button>
+    <mu-button flat slot="right" to="/Login">退出</mu-button>
     {{title}}
   </mu-appbar>
 </template>
