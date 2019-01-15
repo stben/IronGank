45da348ad65341424d290f7407c55886cb9cc7c3
林择旭 <261796040@qq.com>
2019-01-15 17:15:21 +0800

    Complete the teacher's function of removing a student from the queueable list. Ref #127

--- frontend/src/components/PickStuTabsAccepted.vue
+++ frontend/src/components/PickStuTabsAccepted.vue
@@ -1,7 +1,7 @@
 <template>
   <mu-paper class="paper">
     <mu-tabs :value.sync="active" color="white" indicator-color="blue" center class="tabs">
-      <mu-tab style="color: #9e9e9e" to="/teacher/pickStudent/audit">待审核</mu-tab>
+      <mu-tab style="color: #9e9e9e" :to="{ name: 'PickStuAudit', params: {roomNo: roomNo}}">待审核</mu-tab>
       <mu-tab style="color: #03a9f4">已通过</mu-tab>
     </mu-tabs>
     <div v-if="active===1" class="list">
@@ -9,7 +9,7 @@
         <mu-list-row v-for="item in acceptedList" :key="item.stuNo">
           <mu-list-item>
             <mu-list-item-title>姓名：{{item.stuName}}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;学号：{{item.stuNo}}</mu-list-item-title>
-            <mu-button flat color="error" @click="remove($event)">移除</mu-button>
+            <mu-button flat color="error" v-bind:id="item.stuNo" @click="remove($event)">移除</mu-button>
             <span class="removehint">已移除</span>
           </mu-list-item>
           <mu-divider></mu-divider>
@@ -22,7 +22,7 @@
 <script>
 export default {
   name: 'PickStuTabsAccepted',
-  props: ['acceptedList'],
+  props: ['acceptedList', 'roomNo'],
   data() {
     return {
       active: 1
@@ -30,8 +30,21 @@ export default {
   },
   methods: {
     remove(e) {
-      e.currentTarget.style.display = 'none'
-      e.currentTarget.nextElementSibling.style.display = 'inline'
+      let postData = {
+        'roomNo': this.roomNo,
+        'stuNo': e.currentTarget.id,
+        'isAccepted': '0'
+      }
+      let ec = e.currentTarget
+      this.$axios.post('api/teacher/pickStudent',
+        this.$Qs.stringify(postData)
+      )
+        .then((response) => {
+          if (response.data.code === '0000') {
+            ec.style.display = 'none'
+            ec.nextElementSibling.style.display = 'inline'
+          } else alert(response.data.msg)
+        })
     }
   }
 }
