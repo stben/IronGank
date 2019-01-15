5724c77a2878e9e6f049eb759cf9ee3481797ba8
占晨昊 <1405092011@qq.com>
2019-01-16 01:01:43 +0800

    Student Index is under controll. Ref #133

--- server/student/models.py
+++ server/student/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    sid = models.CharField(max_length=20)
+    sid = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
         return User.objects.get(id=self.user_id).username
