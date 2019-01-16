dbe919844b939986062665215566f689eafe5bc4
林择旭 <261796040@qq.com>
2019-01-16 14:20:19 +0800

    Review improvements to pages. Ref #135

--- frontend/src/components/PickStuTabsAudit.vue
+++ frontend/src/components/PickStuTabsAudit.vue
@@ -1,24 +1,24 @@
 <template>
-  <mu-paper class="paper">
-    <mu-tabs :value.sync="active" color="white" indicator-color="blue" center class="tabs">
-      <mu-tab style="color: #03a9f4">待审核</mu-tab>
-      <mu-tab style="color: #9e9e9e" :to="{ name: 'PickStuAccepted', params: {roomNo: roomNo}}">已通过</mu-tab>
+  <div id="PickStuTabsAudit">
+    <mu-tabs :value.sync="active" inverse color="blue" indicator-color="blue" center class="tabs">
+      <mu-tab>待审核</mu-tab>
+      <mu-tab :to="{ name: 'PickStuAccepted', params: {roomNo: roomNo}}">已通过</mu-tab>
     </mu-tabs>
-    <div v-if="active===0" class="list">
-    <mu-list>
-      <mu-list-row v-for="item in auditList" :key="item.stuNo">
-        <mu-list-item>
-          <mu-list-item-title>姓名：{{item.stuName}}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;学号：{{item.stuNo}}</mu-list-item-title>
-          <mu-button flat color="success" v-bind:id="item.stuNo" @click="agree($event)">通过</mu-button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <mu-button flat color="error" v-bind:id="item.stuNo" @click="disagree($event)">拒绝</mu-button>
-          <span class="agreehint">已通过</span>
-          <span class="disagreehint">未通过</span>
-        </mu-list-item>
-        <mu-divider></mu-divider>
-      </mu-list-row>
-    </mu-list>
-    </div>
-  </mu-paper>
+    <mu-paper :z-depth="2" v-if="active===0" class="paper">
+        <mu-data-table :columns="columns" :data="auditList1">
+          <template slot-scope="item">
+            <td>{{item.row.stuNo}}</td>
+            <td>{{item.row.stuName}}</td>
+            <td>
+              <mu-button flat color="success" v-bind:id="item.row.stuNo" @click="agree($event)">通过</mu-button>
+              <mu-button flat color="error" v-bind:id="item.row.stuNo" @click="disagree($event)">拒绝</mu-button>
+              <span class="agreehint">已通过</span>
+              <span class="disagreehint">未通过</span>
+            </td>
+          </template>
+        </mu-data-table>
+    </mu-paper>
+  </div>
 </template>
 
 <script>
@@ -27,7 +27,10 @@ export default {
   props: ['auditList', 'roomNo'],
   data() {
     return {
-      active: 0
+      active: 0,
+      columns: [
+        { title: '学号', name: 'stuNo' }, { title: '姓名', name: 'stuName' }, { title: '操作', name: '', align: 'center' }
+      ]
     }
   },
   methods: {
@@ -72,27 +75,24 @@ export default {
 </script>
 
 <style scoped>
-  .paper{
-    margin: -50px 300px;
-  }
   .tabs{
-    margin: -240px auto;
-    width: 600px;
+    margin: 100px auto;
+    width: 300px;
   }
-  .list{
-    width: 600px;
-    margin: 250px auto;
-  }
+  .paper{
+       margin: -80px auto;
+       width: 750px;
+     }
   .agreehint{
     display: none;
     color: #4caf50;
     width: 80px;
-    margin-right: 70px;
+    margin: 0 80px;
   }
   .disagreehint{
     display: none;
     color: #f44336;
     width: 80px;
-    margin-right: 70px;
+    margin: 0 80px;
   }
 </style>
