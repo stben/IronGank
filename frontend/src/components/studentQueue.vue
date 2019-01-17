dc44c71ac42b21fb736b29ae24dd9479318d9dd9
郑绪祺 <1194792327@qq.com>
2019-01-17 23:36:33 +0800

    Complete the fuction of queue. Ref #159

---
+++ frontend/src/components/studentQueue.vue
@@ -0,0 +1,83 @@
+<template>
+  <div id="queue">
+    {{stuList}}
+    <mu-list class="badge-list-wrap">
+      <mu-list-item v-for="(item, index) in stuList"
+                    :key="index">
+        <mu-list-item-content>
+          <mu-list-item-title>{{item}}</mu-list-item-title>
+        </mu-list-item-content>
+      </mu-list-item>
+    </mu-list>
+    <mu-button font
+               color="primary"
+               @click="changeMyStatus">{{buttonInfo}}</mu-button>
+  </div>
+</template>
+
+<script>
+import socket from 'socket.io-client'
+const SocketInsatnce = socket('http://localhost:3000')
+
+export default {
+  name: 'studentQueue',
+  components: {
+  },
+  data () {
+    return {
+      stuInfoList: [],
+      inQueue: false,
+      buttonInfo: '加入队列',
+      activeStyle: this.queueStatus ? 'disable' : ' color="primary" '
+    }
+  },
+  watch: {
+    inQueue: {
+      handler (newName) {
+        this.buttonInfo = this.inQueue ? '离开队列' : '加入队列'
+      }
+    }
+  },
+  mounted () {
+    let self = this
+    this.buttonInfo = this.inQueue ? '离开队列' : '加入队列'
+    // this.stuList = this.stuList
+    SocketInsatnce.emit('joinRoom', this.roomNo)
+    SocketInsatnce.on('updateList', (data) => {
+      if (data.indexOf(self.myName) !== -1) {
+        self.inQueue = true
+      } else {
+        self.inQueue = false
+      }
+      self.stuList = data
+    })
+  },
+  props: ['stuList', 'myName', 'myNo', 'roomNo', 'roomName', 'queueStatus'],
+  methods: {
+    updateOurList: function () {
+      let data = { 'roomNo': this.roomNo, 'msg': this.stuList }
+      SocketInsatnce.emit('sendMsg', data)
+      console.log(data)
+    },
+    changeMyStatus: function () {
+      if (this.inQueue) {
+        let index = this.stuList.indexOf(this.myName)
+        if (index > -1) {
+          // console.log(this.stuList)
+          this.stuList.splice(index, 1)
+          // console.log(this.stuList)
+        }
+        this.inQueue = !this.inQueue
+      } else {
+        this.inQueue = !this.inQueue
+        this.stuList.push(this.myName)
+      }
+      this.updateOurList()
+      this.sendMsgToParent(this.stuList)
+    },
+    sendMsgToParent: function (data) {
+      this.$emit('listenToChildEvent', data)
+    }
+  }
+}
+</script>
