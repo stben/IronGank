f0011399f760550449207f08c2dee0e9083ad8bb
林择旭 <261796040@qq.com>
2019-01-17 16:54:37 +0800

    Complete the student login screen. Ref #153

--- frontend/src/components/login.vue
+++ frontend/src/components/login.vue
@@ -1,125 +1,16 @@
 <template>
-  <mu-container>
-    <mu-tabs :value.sync="active" inverse color="blue" indicator-color="blue" center>
-      <mu-tab>学生</mu-tab>
-      <mu-tab>教师</mu-tab>
-    </mu-tabs>
-    <div v-if="active===0">
-      <mu-row>
-        <mu-col span="12" class="hint">&nbsp;{{error0}}</mu-col>
-      </mu-row>
-      <mu-form ref="form0" :model="validateForm0" class="formstyle">
-        <mu-form-item prop="username" :rules="usernameRules0">
-          <mu-text-field v-model="validateForm0.username" prop="username" placeholder="学号/手机号"></mu-text-field>
-        </mu-form-item>
-        <mu-form-item prop="password" :rules="passwordRules0">
-          <mu-text-field type="password" v-model="validateForm0.password" prop="password" placeholder="密码"></mu-text-field>
-        </mu-form-item>
-        <mu-form-item class="loginbutton">
-          <mu-button color="primary" @click="submitStudent">登录</mu-button>
-        </mu-form-item>
-        <mu-form-item class="signin">
-          <mu-button flat color="primary" to="/register" small>注册</mu-button>
-        </mu-form-item>
-      </mu-form>
-    </div>
-   <div v-if="active===1">
-     <mu-row>
-       <mu-col span="12" class="hint">&nbsp;{{error1}}</mu-col>
-     </mu-row>
-     <mu-form ref="form1" :model="validateForm1" class="formstyle">
-       <mu-form-item :rules="usernameRules1">
-         <mu-text-field v-model="validateForm1.username" prop="username" rules="passwordRules0" placeholder="工号/邮箱"></mu-text-field>
-       </mu-form-item>
-       <mu-form-item prop="password" :rules="passwordRules1">
-         <mu-text-field type="password" v-model="validateForm1.password" prop="password" rules="passwordRules0" placeholder="密码"></mu-text-field>
-       </mu-form-item>
-       <mu-form-item class="loginbutton">
-         <mu-button color="primary" @click="submitTeacher">登录</mu-button>
-       </mu-form-item>
-     </mu-form>
-   </div>
-  </mu-container>
+  <div id="login"></div>
 </template>
 
 <script>
 export default {
   name: 'login',
-  data () {
-    return {
-      active: 0,
-      error0: '',
-      error1: '',
-      usernameRules0: [
-        { validate: (val) => !!val, message: '必须填写用户名' }
-      ],
-      passwordRules0: [
-        { validate: (val) => !!val, message: '必须填写密码' }
-      ],
-      usernameRules1: [
-        { validate: (val) => !!val, message: '必须填写用户名' }
-      ],
-      passwordRules1: [
-        { validate: (val) => !!val, message: '必须填写密码' }
-      ],
-      validateForm0: {
-        username: '',
-        password: ''
-      },
-      validateForm1: {
-        username: '',
-        password: ''
-      }
-    }
-  },
-  methods: {
-    submitStudent: function () {
-      if (!!this.validateForm0.username && !!this.validateForm0.password) {
-        this.$axios.post('/api/student/studentLogin',
-          this.$Qs.stringify(this.validateForm0)
-        )
-          .then((response) => {
-            if (response.data.code === '0000') {
-              this.$router.push('/student/index')
-            } else {
-              alert(response.data.msg)
-            }
-          })
-      }
-    },
-    submitTeacher: function () {
-      if (this.validateForm0.username && this.validateForm0.password) {
-        this.$axios.post('/api/teacher/teacherLogin',
-          this.$Qs.stringify(this.validateForm1)
-        )
-          .then((response) => {
-            if (response.data.code === '0000') {
-              this.$router.push('/teacher/index')
-            } else {
-              alert(response.data.msg)
-            }
-          })
-      }
-    }
+  mounted() {
+    this.$router.push('/student/login')
   }
 }
 </script>
 
 <style scoped>
-  .formstyle{
-    display:flex;
-    flex-direction: column;
-    width:350px;
-    margin: -10px auto;
-  }
-  .hint{
-    color: red;
-    margin: 30px auto;
-  }
-  .loginbutton{
-    margin:30px auto;
-  }
-  .signin{
-    margin: -78px 250px;
-  }
+
 </style>
---
+++ frontend/src/components/studentLogin.vue
@@ -0,0 +1,113 @@
+<template>
+  <div id="studentLogin">
+    <img src="../assets/background.jpg" class="img">
+    <mu-paper :z-depth="4" class="board">
+      <span class="text">Iron Gank</span>
+      <mu-tabs :value.sync="active" color="white" indicator-color="blue" center>
+        <mu-tab style="color: #03a9f4">学生</mu-tab>
+        <mu-tab style="color: #78909c" to="/teacher/login">教师</mu-tab>
+      </mu-tabs>
+      <div v-if="active===0">
+        <mu-form ref="form0" :model="validateForm0" class="formstyle">
+          <mu-form-item prop="username" :rules="usernameRules0">
+            <mu-text-field v-model="validateForm0.username" prop="username" placeholder="学号/手机号" class="input"></mu-text-field>
+          </mu-form-item>
+          <mu-form-item prop="password" :rules="passwordRules0">
+            <mu-text-field type="password" v-model="validateForm0.password" prop="password" placeholder="密码" class="input"></mu-text-field>
+          </mu-form-item>
+          <mu-form-item class="loginbutton">
+            <mu-button color="primary" @click="submitStudent">登录</mu-button>
+          </mu-form-item>
+          <mu-form-item class="signin">
+            <mu-button flat color="primary" to="/register" small>注册</mu-button>
+          </mu-form-item>
+        </mu-form>
+      </div>
+    </mu-paper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'studentLogin',
+  data () {
+    return {
+      active: 0,
+      error0: '',
+      error1: '',
+      usernameRules0: [
+        { validate: (val) => !!val, message: '必须填写用户名' }
+      ],
+      passwordRules0: [
+        { validate: (val) => !!val, message: '必须填写密码' }
+      ],
+      usernameRules1: [
+        { validate: (val) => !!val, message: '必须填写用户名' }
+      ],
+      passwordRules1: [
+        { validate: (val) => !!val, message: '必须填写密码' }
+      ],
+      validateForm0: {
+        username: '',
+        password: ''
+      },
+      validateForm1: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    submitStudent: function () {
+      if (!!this.validateForm0.username && !!this.validateForm0.password) {
+        this.$axios.post('/api/student/studentLogin',
+          this.$Qs.stringify(this.validateForm0)
+        )
+          .then((response) => {
+            if (response.data.code === '0000') {
+              this.$router.push('/student/index')
+            } else {
+              alert(response.data.msg)
+            }
+          })
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .img{
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+  }
+  .board{
+    margin: 120px auto;
+    width: 390px;
+    height: 380px;
+  }
+  .text{
+    font-size: 40px;
+    color: grey;
+  }
+  .formstyle{
+    display:flex;
+    flex-direction: column;
+    width:350px;
+    margin: 50px auto;
+  }
+  .input{
+    margin-left: 20px;
+    width: 320px;
+  }
+  .loginbutton{
+    margin:30px auto;
+  }
+  .signin{
+    margin: -78px 250px;
+  }
+</style>
